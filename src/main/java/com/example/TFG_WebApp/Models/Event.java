4333052a4e00510177bfeeed7daf796236fdf12d
SamuelAguiar2020 <s.aguiar1.2020@alumnos.urjc.es>
2024-10-17 11:59:19 +0200

    Configuracion de los direccionamientos
    Definimos los direccionamientos con la pestaña del calendario de eventos y algnos mas pasos entre pestañas que tenian fallos

--- src/main/java/com/example/TFG_WebApp/Models/Event.java
+++ src/main/java/com/example/TFG_WebApp/Models/Event.java
@@ -22,4 +22,59 @@ public class Event {
     private String link_image = ".../static/images/FAM.jpg";
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<Discipline> getDisciplines() {
+        return disciplines;
+    }
+
+    public void setDisciplines(List<Discipline> disciplines) {
+        this.disciplines = disciplines;
+    }
+
+    public List<Results> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Results> results) {
+        this.results = results;
+    }
+
+    public Boolean getOrganizers() {
+        return organizers;
+    }
+
+    public void setOrganizers(Boolean organizers) {
+        this.organizers = organizers;
+    }
+
+    public String getLink_image() {
+        return link_image;
+    }
+
+    public void setLink_image(String link_image) {
+        this.link_image = link_image;
+    }
 }
