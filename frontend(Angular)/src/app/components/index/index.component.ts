37228f0fbe47d1e9208980b13872fbd38c3ce636
SamuelAguiar2020 <s.aguiar1.2020@alumnos.urjc.es>
2024-12-29 21:02:24 +0100

    index design in angular
    design of the index component in Angular, to define the functionality of the initial page of the application.

---
+++ frontend(Angular)/src/app/components/index/index.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent {
+  toggleMenu() {
+    const menu = document.getElementById('dropdown-menu');
+    if (menu) {
+      menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+    }
+  }
+}
