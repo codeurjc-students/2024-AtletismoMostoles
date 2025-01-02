7e0f37cefee532e659fe38411a2694f6a747ddb6
SamuelAguiar2020 <s.aguiar1.2020@alumnos.urjc.es>
2025-01-02 18:03:35 +0100

    clubmembers component definition
    Definition of the html files, text script and the style sheet of the clubmemebers (coaches) component.

--- frontend(Angular)/src/app/app.routes.ts
+++ frontend(Angular)/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { RankingComponent } from './components/ranking/ranking.component';
+import {ClubMembersComponent} from './components/clubmembers/clubmembers.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'ranking', component: RankingComponent },
+  { path: 'miembros', component: ClubMembersComponent}
 ];
 
 @NgModule({
--- frontend(Angular)/src/app/components/clubmembers/clubmembers.component.ts
+++ frontend(Angular)/src/app/components/clubmembers/clubmembers.component.ts
@@ -1,11 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgForOf} from '@angular/common';
+
+interface Coaches {
+  nombre: string;
+  apellido: string;
+  numeroLicencia: string;
+  disciplina: string;
+}
 
 @Component({
   selector: 'app-clubmembers',
-  imports: [],
   templateUrl: './clubmembers.component.html',
-  styleUrl: './clubmembers.component.css'
+  standalone: true,
+  imports: [
+    FormsModule,
+    RouterLink,
+    NgForOf,
+    RouterOutlet
+  ],
+  styleUrls: ['./clubmembers.component.css']
 })
-export class ClubmembersComponent {
+export class ClubMembersComponent implements OnInit {
 
+  filters = {
+    nombre: '',
+    apellido: '',
+    numeroLicencia: '',
+    disciplina: ''
+  };
+  coaches: Coaches[] = [];
+  paginatedCoaches: Coaches[] = [];
+  currentPage = 1;
+  itemsPerPage = 10;
+  totalPages = 1;
+
+  constructor() {
+    this.updatePagination(); // Llamada inicial para llenar paginatedAtletas
+  }
+  /*
+  private apiUrl = 'http://localhost:8080/api/coaches';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadEntrenadores();
+  }
+
+  loadEntrenadores(): void {
+    const params: any = {
+      page: this.currentPage - 1,
+      size: 10,
+      ...this.filters
+    };
+
+    this.http.get<any>(this.apiUrl, { params }).subscribe(
+      response => {
+        this.entrenadores = response.content;
+        this.totalPages = response.totalPages;
+      },
+      error => {
+        console.error('Error al cargar entrenadores:', error);
+      }
+    );
+  }*/
+
+  applyFilters(): void {
+    this.currentPage = 1;
+    //this.loadEntrenadores();
+  }
+
+  updatePagination() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = this.currentPage * this.itemsPerPage;
+    this.paginatedCoaches = this.coaches.slice(start, end);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      //this.loadEntrenadores();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      //this.loadEntrenadores();
+    }
+  }
+
+  toggleMenu() {
+    const menu = document.getElementById('dropdown-menu');
+    if (menu) {
+      menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+    }
+  }
+  ngOnInit(): void {
+  }
 }
