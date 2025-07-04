8bf14a3f433e5022e97c4aca293325575183facb
SamuelAguiar2020 <s.aguiar1.2020@alumnos.urjc.es>
2025-07-04 13:59:34 +0200

    service1  webSocket update

--- service1-backend/src/main/java/com/example/service1/Listeners/PdfConfirmationListener.java
+++ service1-backend/src/main/java/com/example/service1/Listeners/PdfConfirmationListener.java
@@ -1,4 +1,23 @@
 package com.example.service1.Listeners;
 
+import com.example.service1.DTO.PdfConfirmationMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PdfConfirmationListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public PdfConfirmationListener(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @RabbitListener(queues = "pdf.confirmation.queue")
+    public void handlePdfConfirmation(PdfConfirmationMessage confirmation) {
+        String destino = "/topic/pdf/" + confirmation.getAtletaId();
+        messagingTemplate.convertAndSend(destino, confirmation);
+        System.out.println("📨 Enviado PDF a WebSocket para atleta " + confirmation.getAtletaId());
+    }
 }
